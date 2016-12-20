54c73fc27bb7e95647cbf49242e85fb57d68feae
DaveXuYuanfu <635468167@qq.com>
2016-12-20 15:13:52 +0800

    改为一对多通信
    监听客户端请求，据客户计数创建服务线程并启动。

--- Server主机端.java
+++ Server主机端.java
@@ -1,4 +1,4 @@
-package server;
+﻿package server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,58 +7,73 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class server {
-
+public class server_mult {
+	public static int num=1;//客户端计数
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        try{
-        	ServerSocket server=null;//创建服务器端
-        	System.out.println("端口1211创建成功!");
-		try {
-			server=new ServerSocket(1211);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("端口创建错误:"+e);
-			System.exit(1);
-			
-		}
-		Socket client=null;
-		try{
-			client=server.accept();//创建客户端
-		}catch (Exception e){
-			System.out.println("客户端向服务器接受请求失败!");
-			System.exit(-1);
-		}
-        String inputString="test";
-        BufferedReader is=new BufferedReader(new InputStreamReader(client.getInputStream()));//IS从服务器端接收
-        PrintWriter os=new PrintWriter(client.getOutputStream());
-        BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("客户端发送的消息为:"+is.readLine());
-        
-      
-        while (inputString!=null&&!inputString.trim().equals("quit")){
+        ServerSocket serverSocket=null;
+        Socket client=null;
+        while(true){
         	
-        	
-        	System.out.println("服务器想要发送消息:");
-        	inputString=sin.readLine();
-        	
-        	System.out.println("服务器发送的消息为:"+inputString);
-        	os.println(inputString);
-        	os.flush();
-        	System.out.println("客户端发送的消息为:"+is.readLine());
-        	//inputString=sin.readLine();
+        		try {
+        			System.out.println("端口1211创建成功");
+					serverSocket=new ServerSocket(1211);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Error:"+e);
+					System.exit(-1);
+				}
+        		try {
+					client=serverSocket.accept();//产生一个socket对象
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					System.out.println("接受请求失败!");
+					System.exit(-1);
+				}
+        		System.out.println("客户端"+server_mult.num+"号登陆");
+        		ServerThread st=new ServerThread(client);
+        		Thread t=new Thread(st);
+        		t.start();//监听到客户请求，据客户技术创建服务线程并启动
+        	    try{serverSocket.close();}
+        	    catch (IOException e){ 
+        	    System.out.println("关闭失败!");
+        	    }
+        	    num++;
+         }
         }
-        os.close();
-        is.close();
-        client.close();
-        server.close();
-        System.out.println("聊天结束!");
-        
-	}catch(Exception e){
-		System.out.println("Error:"+e);
-	}
-	}//服务器端程序
-
 }
+
+      class ServerThread implements Runnable{
+    	  private Socket client;
+    	  public ServerThread(Socket client){
+    		  this.client=client;
+    	  }
+    	  public void run()
+    	  {
+    		  try{
+    			  BufferedReader is=new BufferedReader(new InputStreamReader(client.getInputStream()));
+    			  PrintWriter os=new PrintWriter(client.getOutputStream());
+    			  BufferedReader sin=new BufferedReader(new InputStreamReader(System.in));
+    			  System.out.println("客户端: "+is.readLine());
+    			  String inputString=sin.readLine();
+    			  while(inputString!=null&&!inputString.trim().equals("quit")){
+    				  os.println(inputString);
+    				  os.flush();
+    				  System.out.println("服务器发送的消息是:"+inputString);
+    				  System.out.println("客户端发送的消息是:"+is.readLine());
+    				  inputString= sin.readLine();
+    			  }
+    			  os.close();
+    			  is.close();
+    			  client.close();
+    			  System.out.println("聊天结束!");
+    			  
+    		  }catch (IOException e){
+    			  e.printStackTrace();
+    		  }
+    		  
+    	  }
+        
+	}
 
 
