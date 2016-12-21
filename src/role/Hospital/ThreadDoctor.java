1cfaa673b79b5d30012122cada54c66ea79d33b6
Mozgh <zhanggh430@163.com>
2016-12-21 14:52:13 +0800

    12.21.2

--- src/role/Hospital/ThreadDoctor.java
+++ src/role/Hospital/ThreadDoctor.java
@@ -4,7 +4,20 @@ import java.util.Hashtable;
 
 public class ThreadDoctor extends ServerThread {
 	String init_sql="SELECT * FROM Patient WHERE Do.id="+id;	//初始sql操作语句，查询医生本人名下挂号的病人
-	//线程工作，查询
+	public void run(){
+		this.start();
+		while(conn_state)
+		{
+			this.work();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	//线程工作内容
 	public void work(){
 		while(conn_state)
 		{
@@ -23,5 +36,8 @@ public class ThreadDoctor extends ServerThread {
 		String illness=client.get(6).toString();
 		sql_command="UPDATE Patient SET Pa_illness='"+illness+"',Pa_prescription='"+prescription
 				+"' WHERE Pa_id='"+patientid+"'";
+	}
+	public static void main(String[] args) {
+		ThreadDoctor doctor;
 	}
 }
