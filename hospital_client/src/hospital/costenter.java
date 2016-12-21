5d0299988591ed3469cb81e8f2cab1fc7aabc7cf
pengyongde <297857448@qq.com>
2016-12-21 22:13:48 +0800

    client GUI

---
+++ hospital_client/src/hospital/costenter.java
@@ -0,0 +1,89 @@
+package hospital;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class costenter extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					costenter frame = new costenter();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public costenter() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("\u8D26\u53F7");
+		label.setBounds(93, 78, 54, 15);
+		contentPane.add(label);
+		
+		JLabel label_1 = new JLabel("\u5BC6\u7801");
+		label_1.setBounds(93, 103, 54, 15);
+		contentPane.add(label_1);
+		
+		textField = new JTextField();
+		textField.setBounds(132, 75, 104, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(132, 100, 104, 21);
+		contentPane.add(textField_1);
+		
+		JButton btnNewButton = new JButton("\u767B\u5F55");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cost newframe = new cost();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(86, 158, 93, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u8FD4\u56DE\u9996\u9875");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sys_hospital newframe = new sys_hospital();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(213, 158, 93, 23);
+		contentPane.add(btnNewButton_1);
+	}
+
+}
---
+++ hospital_client/src/hospital/guahao.java
@@ -0,0 +1,135 @@
+package hospital;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JSpinner;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import java.awt.Color;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+
+public class guahao extends JFrame {
+
+	private JPanel f3;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					guahao frame = new guahao();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public guahao() {
+		setTitle("\u586B\u5199\u4E2A\u4EBA\u4FE1\u606F");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		f3 = new JPanel();
+		f3.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(f3);
+		f3.setLayout(null);
+		
+		JLabel label = new JLabel(" \u59D3\u540D");
+		label.setBounds(55, 20, 40, 20);
+		f3.add(label);
+		
+		textField = new JTextField();
+		textField.setBounds(90, 20, 100, 20);
+		f3.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel(" \u6027\u522B");
+		lblNewLabel.setBounds(55, 40, 40, 20);
+		f3.add(lblNewLabel);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(90, 40, 100, 20);
+		f3.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel(" \u5E74\u9F84");
+		lblNewLabel_1.setBounds(55, 60, 40, 20);
+		f3.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(" \u79D1\u5BA4\r\n");
+		lblNewLabel_2.setBounds(55, 80, 40, 20);
+		f3.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel(" \u533B\u751F");
+		lblNewLabel_3.setBounds(55, 100, 40, 20);
+		f3.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel(" \u75C7\u72B6");
+		lblNewLabel_4.setBounds(55, 120, 40, 20);
+		f3.add(lblNewLabel_4);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(90, 60, 100, 20);
+		f3.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnNewButton = new JButton("\u786E\u5B9A");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				guahao2 newframe = new guahao2();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(90, 213, 93, 30);
+		f3.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u53CD\u56DE\u9996\u9875");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sys_hospital newframe = new sys_hospital();
+				newframe.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(249, 213, 93, 30);
+		f3.add(btnNewButton_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"    \u79D1\u5BA41", "    \u79D1\u5BA42"}));
+		comboBox.setForeground(Color.BLACK);
+		comboBox.setToolTipText("");
+		comboBox.setBounds(90, 80, 100, 20);
+		f3.add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"\u79D1\u5BA41 \u533B\u751FA", "\u79D1\u5BA41 \u533B\u751FA", "\u79D1\u5BA41 \u533B\u751FA", "\u79D1\u5BA42 \u533B\u751FA", "\u79D1\u5BA42 \u533B\u751FA", "\u79D1\u5BA42 \u533B\u751FA"}));
+		comboBox_1.setBounds(90, 100, 100, 20);
+		f3.add(comboBox_1);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setBounds(90, 120, 200, 60);
+		f3.add(textPane);
+	}
+}
